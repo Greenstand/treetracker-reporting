c911fb2c6e0078a8aaef6c8a86cbb3ab397d2e4c
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-11-11 15:04:31 -0800

    feat: update reporting schema

--- database/migrations/sqls/20210923184249-capture-denormalized-up.sql
+++ database/migrations/sqls/20210923184249-capture-denormalized-up.sql
@@ -1,18 +1,20 @@
 CREATE TABLE capture_denormalized
 (
     capture_uuid uuid NOT NULL PRIMARY KEY,
+    capture_created_at timestamptz NOT NULL,
     planter_first_name varchar NOT NULL,
     planter_last_name varchar NOT NULL,
     planter_identifier varchar NOT NULL,
-    created_at timestamptz NOT NULL,
     lat numeric NOT NULL,
     lon numeric NOT NULL,
     note varchar NOT NULL,
     approved boolean NOT NULL,
-    planting_organization varchar NOT NULL,
+    planting_organization_uuid varchar NOT NULL,
+    planting_organization_name uuid NOT NULL,
     date_paid timestamptz NOT NULL,
     paid_by varchar NOT NULL,
     payment_local_amt numeric NOT NULL,
     species varchar NOT NULL,
-    token_id uuid NOT NULL
+    token_id uuid NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT NOW()
 );
