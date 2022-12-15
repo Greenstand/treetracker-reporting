137957782e9529280c5289872d26fb4eb888f23f
Olojakpoke Daniel <holodja@hotmail.com>
2022-12-15 03:49:23 +0100

    fix: minor updates

--- server/models/Tree.js
+++ server/models/Tree.js
@@ -1,152 +1,148 @@
 const TreeRepository = require('../repositories/TreeRepository');
 
 class Tree {
-    constructor(session) {
-        this._treeRepository = new TreeRepository(session);
-    }
+  constructor(session) {
+    this._treeRepository = new TreeRepository(session);
+  }
 
-    static Tree({
-        tree_uuid,
-        tree_created_at,
-        planter_first_name,
-        planter_last_name,
-        planter_identifier,
-        created_at,
-        lat,
-        lon,
-        note,
-        planting_organization_uuid,
-        planting_organization_name,
-        species,
-      }) {
-        return Object.freeze({
-          tree_uuid,
-          tree_created_at,
-          planter_first_name,
-          planter_last_name,
-          planter_identifier,
-          created_at,
-          lat,
-          lon,
-          note,
-          planting_organization_uuid,
-          planting_organization_name,
-          species,
-        });
-      }
+  static Tree({
+    tree_uuid,
+    tree_created_at,
+    planter_first_name,
+    planter_last_name,
+    planter_identifier,
+    created_at,
+    lat,
+    lon,
+    note,
+    planting_organization_uuid,
+    planting_organization_name,
+    species,
+  }) {
+    return Object.freeze({
+      tree_uuid,
+      tree_created_at,
+      planter_first_name,
+      planter_last_name,
+      planter_identifier,
+      created_at,
+      lat,
+      lon,
+      note,
+      planting_organization_uuid,
+      planting_organization_name,
+      species,
+    });
+  }
 
-      async getTrees(filter, limitOptions) {
-        const filterCriteria = {
-          ...filter,
-          ...(filter?.since && { created_at: new Date(filter.since) }),
-          ...(filter?.since_tree_created_at && {
-            tree_created_at: new Date(filter.since_tree_created_at),
-          }),
-          ...(filter?.since_date_paid && {
-            date_paid: new Date(filter.since_date_paid),
-          }),
-        };
-    
-        delete filterCriteria.since;
-        delete filterCriteria.since_tree_created_at;
-        delete filterCriteria.since_date_paid;
-    
-        const { trees, count } = await this._treeRepository.getTrees(
-          filterCriteria,
-          limitOptions,
-        );
-    
-        return {
-          trees: trees.map((row) => {
-            return this.constructor.Tree({ ...row });
-          }),
-          totalCount: count,
-        };
-      }
+  async getTrees(filter, limitOptions) {
+    const filterCriteria = {
+      ...filter,
+      ...(filter?.since && { created_at: new Date(filter.since) }),
+      ...(filter?.since_tree_created_at && {
+        tree_created_at: new Date(filter.since_tree_created_at),
+      }),
+    };
 
-      static generateFormattedResponse({
-        averageTreesPerPlanter = undefined,
-        topPlanters = [],
-        totalGrowers = undefined,
-        topGrowersPerOrganizatino = [],
-        totalSpecies = undefined,
-        topSpecies = [],
-        totalTrees = undefined,
-        topTrees = [],
-        averageTreesPerPlanterPerOrganization = undefined,
-        topAverageTreesPerPlanterPerOrganization = [],
-        lastUpdated = undefined,
-        genderCount = [],
-      }) {
-        const planters = {
-          total: totalGrowers,
-          planters: topGrowersPerOrganizatino.map(
-            ({ planting_organization_name, count }) => {
-              return { name: planting_organization_name, number: count };
-            },
-          ),
-        };
-    
-        const species = {
-          total: totalSpecies,
-          species: topSpecies.map(({ species: s, count }) => {
-            return { name: s, number: count };
-          }),
-        };
-    
-        const trees = {
-          total: totalTrees,
-          trees: topTrees.map(({ planting_organization_name, count }) => {
-            return { name: planting_organization_name, number: count };
-          }),
-        };
-    
-        const top_planters = {
-          average: Math.round(averageTreesPerPlanter),
-          top_planters: topPlanters.map(
-            ({ planter_first_name, planter_last_name, count }) => {
-              return {
-                name: `${planter_first_name} ${planter_last_name}`,
-                number: count,
-              };
-            },
-          ),
-        };
-    
-        const trees_per_planters = {
-          average: Math.round(averageTreesPerPlanterPerOrganization),
-          trees_per_planters: topAverageTreesPerPlanterPerOrganization.map(
-            ({ planting_organization_name, averagetreesperplanters }) => {
-              return {
-                name: planting_organization_name,
-                number: Math.round(averagetreesperplanters),
-              };
-            },
-          ),
-        };
-    
-        const last_updated_at = lastUpdated;
-    
-        const totalCount = genderCount.reduce((a, b) => a + +b.count, 0);
-    
-        const gender_details = genderCount.map(({ gender, count }) => {
+    delete filterCriteria.since;
+    delete filterCriteria.since_tree_created_at;
+
+    const { trees, count } = await this._treeRepository.getTrees(
+      filterCriteria,
+      limitOptions,
+    );
+
+    return {
+      trees: trees.map((row) => {
+        return this.constructor.Tree({ ...row });
+      }),
+      totalCount: count,
+    };
+  }
+
+  static generateFormattedResponse({
+    averageTreesPerPlanter = undefined,
+    topPlanters = [],
+    totalGrowers = undefined,
+    topGrowersPerOrganizatino = [],
+    totalSpecies = undefined,
+    topSpecies = [],
+    totalTrees = undefined,
+    topTrees = [],
+    averageTreesPerPlanterPerOrganization = undefined,
+    topAverageTreesPerPlanterPerOrganization = [],
+    lastUpdated = undefined,
+    genderCount = [],
+  }) {
+    const planters = {
+      total: totalGrowers,
+      planters: topGrowersPerOrganizatino.map(
+        ({ planting_organization_name, count }) => {
+          return { name: planting_organization_name, number: count };
+        },
+      ),
+    };
+
+    const species = {
+      total: totalSpecies,
+      species: topSpecies.map(({ species: s, count }) => {
+        return { name: s, number: count };
+      }),
+    };
+
+    const trees = {
+      total: totalTrees,
+      trees: topTrees.map(({ planting_organization_name, count }) => {
+        return { name: planting_organization_name, number: count };
+      }),
+    };
+
+    const top_planters = {
+      average: Math.round(averageTreesPerPlanter),
+      top_planters: topPlanters.map(
+        ({ planter_first_name, planter_last_name, count }) => {
           return {
-            name: gender,
+            name: `${planter_first_name} ${planter_last_name}`,
             number: count,
-            percentage: Math.round((+count / totalCount) * 100),
           };
-        });
-    
-        return {
-          planters,
-          species,
-          trees,
-          top_planters,
-          trees_per_planters,
-          last_updated_at,
-          gender_details: { total: totalGrowers, gender_details },
-        };
-      }
+        },
+      ),
+    };
+
+    const trees_per_planters = {
+      average: Math.round(averageTreesPerPlanterPerOrganization),
+      trees_per_planters: topAverageTreesPerPlanterPerOrganization.map(
+        ({ planting_organization_name, averageTreesPerPlanters }) => {
+          return {
+            name: planting_organization_name,
+            number: Math.round(averageTreesPerPlanters),
+          };
+        },
+      ),
+    };
+
+    const last_updated_at = lastUpdated;
+
+    const totalCount = genderCount.reduce((a, b) => a + +b.count, 0);
+
+    const gender_details = genderCount.map(({ gender, count }) => {
+      return {
+        name: gender,
+        number: count,
+        percentage: Math.round((+count / totalCount) * 100),
+      };
+    });
+
+    return {
+      planters,
+      species,
+      trees,
+      top_planters,
+      trees_per_planters,
+      last_updated_at,
+      gender_details: { total: totalGrowers, gender_details },
+    };
+  }
 
   async getTreeStatistics(filter) {
     const {
--- server/repositories/TreeRepository.js
+++ server/repositories/TreeRepository.js
@@ -1,200 +1,167 @@
 const BaseRepository = require('./BaseRepository');
 
 class TreeRepository extends BaseRepository {
-    constructor(session) {
-        super('tree_denormalized', session);
-        this._tableName = 'tree_denormalized';
-        this._session = session;
+  constructor(session) {
+    super('tree_denormalized', session);
+    this._tableName = 'tree_denormalized';
+    this._session = session;
+  }
+
+  async getTrees(filter, { limit, offset, order, orderBy }) {
+    const whereBuilder = function (object, builder) {
+      const result = builder;
+      const filterObject = { ...object };
+      if (filterObject.created_at) {
+        result.where('created_at', '>=', filterObject.created_at);
+        delete filterObject.created_at;
+      }
+      if (filterObject.tree_created_at) {
+        result.where('tree_created_at', '>=', filterObject.tree_created_at);
+        delete filterObject.tree_created_at;
+      }
+      result.where(filterObject);
+    };
+    let promise = this._session
+      .getDB()(this._tableName)
+      .where((builder) => whereBuilder(filter, builder));
+    if (limit) {
+      promise = promise.limit(limit);
+    }
+    if (offset) {
+      promise = promise.offset(offset);
+    }
+    if (orderBy) {
+      promise = promise.orderBy(orderBy, order);
     }
 
-    async getTrees(filter, { limit, offset, order, orderBy }) {
-        const whereBuilder = function (object, builder) {
-            const result = builder;
-            const filterObject = { ...object };
-            if (filterObject.created_at) {
-              result.where('created_at', '>=', filterObject.created_at);
-              delete filterObject.created_at;
-            }
-            if (filterObject.tree_created_at) {
-              result.where(
-                'tree_created_at',
-                '>=',
-                filterObject.tree_created_at,
-              );
-              delete filterObject.tree_created_at;
-            }
-            result.where(filterObject);
-          };
-          let promise = this._session
-            .getDB()(this._tableName)
-            .where((builder) => whereBuilder(filter, builder));
-          if (limit) {
-            promise = promise.limit(limit);
-          }
-          if (offset) {
-            promise = promise.offset(offset);
-          }
-          if (orderBy) {
-            promise = promise.orderBy(orderBy, order);
-          }
-      
-          const count = await this._session
-            .getDB()(this._tableName)
-            .count('*')
-            .where((builder) => whereBuilder(filter, builder));
-          return { trees: await promise, count: +count[0].count };        
-    }
+    const count = await this._session
+      .getDB()(this._tableName)
+      .count('*')
+      .where((builder) => whereBuilder(filter, builder));
+    return { trees: await promise, count: +count[0].count };
+  }
 
-    async getStatistics(filter, options = { limit: 3, offset: 0 }) {
-        const knex = this._session.getDB();
-    
-        const whereBuilder = function (object, builder) {
-          const result = builder;
-          const filterObject = { ...object };
-          delete filterObject.card_title;
-          if (filterObject.tree_created_start_date) {
-            result.where(
-              'tree_created_at',
-              '>=',
-              filterObject.tree_created_start_date,
-            );
-            delete filterObject.tree_created_start_date;
-          }
-          if (filterObject.tree_created_end_date) {
-            result.where(
-              'tree_created_at',
-              '<=',
-              filterObject.tree_created_end_date,
-            );
-            delete filterObject.tree_created_end_date;
-          }
-          if (filterObject.planting_organization_uuid) {
-            const stakeholderRelationshipQuery = knex.raw(
-              `SELECT sg.stakeholder_id from stakeholder.getStakeholderChildren(?) sg 
+  async getStatistics(filter, options = { limit: 3, offset: 0 }) {
+    const knex = this._session.getDB();
+
+    const whereBuilder = function (object, builder) {
+      const result = builder;
+      const filterObject = { ...object };
+      delete filterObject.card_title;
+      if (filterObject.tree_created_start_date) {
+        result.where(
+          'tree_created_at',
+          '>=',
+          filterObject.tree_created_start_date,
+        );
+        delete filterObject.tree_created_start_date;
+      }
+      if (filterObject.tree_created_end_date) {
+        result.where(
+          'tree_created_at',
+          '<=',
+          filterObject.tree_created_end_date,
+        );
+        delete filterObject.tree_created_end_date;
+      }
+      if (filterObject.planting_organization_uuid) {
+        const stakeholderRelationshipQuery = knex.raw(
+          `SELECT sg.stakeholder_id from stakeholder.getStakeholderChildren(?) sg 
                join stakeholder.stakeholder ss on ss.id = sg.stakeholder_id 
                where ss.type = 'Organization'`,
-              [filterObject.planting_organization_uuid],
-            );
-            result.whereIn(
-              'planting_organization_uuid',
-              stakeholderRelationshipQuery,
-            );
-            delete filterObject.planting_organization_uuid;
-          }
-          result.where(filterObject);
-        };
-    
-        // total number of growers
-        const totalGrowersQuery = knex(this._tableName)
-          .count('* as totalPlanters')
-          .from(function () {
-            this.distinct(
-              'planter_first_name',
-              'planter_last_name',
-              'planter_identifier',
-            )
-              .from('tree_denormalized')
-              .where((builder) => whereBuilder(filter, builder))
-              .as('planters');
-          });
-    
-        // total number of growers per organization
-        const topGrowersPerOrganizatinoQuery = knex(this._tableName)
-          .select(knex.raw('planting_organization_name, count(*) as count'))
-          .from(function () {
-            this.distinct(
-              'planter_first_name',
-              'planter_last_name',
-              'planter_identifier',
-              'planting_organization_name',
-              'planting_organization_uuid',
-            )
-              .from('tree_denormalized')
-              .where((builder) => whereBuilder(filter, builder))
-              .groupBy(
-                'planting_organization_uuid',
-                'planting_organization_name',
-                'planter_first_name',
-                'planter_last_name',
-                'planter_identifier',
-              )
-              .as('planters');
-          })
-          .groupBy('planting_organization_name', 'planting_organization_uuid')
-          .orderBy('count', 'desc')
-          .limit(options.limit)
-          .offset(options.offset);
-    
-        const topPlantersQuery = knex(this._tableName)
-          .select(
-            knex.raw('planter_first_name, planter_last_name, count(*) as count'),
-          )
-          .where((builder) => whereBuilder(filter, builder))
-          .groupBy('planter_first_name', 'planter_last_name', 'planter_identifier')
-          .orderBy('count', 'desc')
-          .limit(options.limit)
-          .offset(options.offset);
-    
-        const averageTreePerPlanterQuery = knex(this._tableName)
-          .avg('totalPlanters')
-          .from(function () {
-            this.count('* as totalPlanters')
-              .from('tree_denormalized')
-              .where((builder) => whereBuilder(filter, builder))
-              .groupBy(
-                'planter_first_name',
-                'planter_last_name',
-                'planter_identifier',
-              )
-              .as('planters');
-          });
-    
-        const totalSpeciesQuery = knex(this._tableName)
-          .where((builder) => whereBuilder(filter, builder))
-          .countDistinct('species as totalSpecies');
-    
-        const topSpeciesQuery = knex(this._tableName)
-          .select(knex.raw('species, count(*) as count'))
-          .where((builder) => whereBuilder(filter, builder))
-          .whereNotNull('species')
-          .groupBy('species')
-          .orderBy('count', 'desc')
-          .limit(options.limit)
-          .offset(options.offset);
-    
-        const averageTreesPerPlanterPerOrganizationQuery = knex(this._tableName)
-          .avg('averageTreesPerPlanters')
-          .from(function () {
-            this.avg('count as averageTreesPerPlanters')
-              .from(function () {
-                this.select(
-                  knex.raw(
-                    `count(*) as count, planting_organization_name, planting_organization_uuid`,
-                  ),
-                )
-                  .from('tree_denormalized')
-                  .where((builder) => whereBuilder(filter, builder))
-                  .groupBy(
-                    'planting_organization_uuid',
-                    'planting_organization_name',
-                    'planter_first_name',
-                    'planter_last_name',
-                    'planter_identifier',
-                  )
-                  .as('plantersCount');
-              })
-              .groupBy('planting_organization_name', 'planting_organization_uuid')
-              .as('plantersAverage');
-          });
-    
-        const topAverageTreesPerPlanterPerOrganizationQuery = knex(
-          this._tableName,
+          [filterObject.planting_organization_uuid],
+        );
+        result.whereIn(
+          'planting_organization_uuid',
+          stakeholderRelationshipQuery,
+        );
+        delete filterObject.planting_organization_uuid;
+      }
+      result.where(filterObject);
+    };
+
+    // total number of growers
+    const totalGrowersQuery = knex(this._tableName)
+      .count('* as totalPlanters')
+      .from(function () {
+        this.distinct(
+          'planter_first_name',
+          'planter_last_name',
+          'planter_identifier',
         )
-          .select(
-            knex.raw(
-              'planting_organization_name, avg(count) as averagetreesperplanters',
-            ),
+          .from('tree_denormalized')
+          .where((builder) => whereBuilder(filter, builder))
+          .as('planters');
+      });
+
+    // total number of growers per organization
+    const topGrowersPerOrganizatinoQuery = knex(this._tableName)
+      .select(knex.raw('planting_organization_name, count(*) as count'))
+      .from(function () {
+        this.distinct(
+          'planter_first_name',
+          'planter_last_name',
+          'planter_identifier',
+          'planting_organization_name',
+          'planting_organization_uuid',
+        )
+          .from('tree_denormalized')
+          .where((builder) => whereBuilder(filter, builder))
+          .groupBy(
+            'planting_organization_uuid',
+            'planting_organization_name',
+            'planter_first_name',
+            'planter_last_name',
+            'planter_identifier',
           )
+          .as('planters');
+      })
+      .groupBy('planting_organization_name', 'planting_organization_uuid')
+      .orderBy('count', 'desc')
+      .limit(options.limit)
+      .offset(options.offset);
+
+    const topPlantersQuery = knex(this._tableName)
+      .select(
+        knex.raw('planter_first_name, planter_last_name, count(*) as count'),
+      )
+      .where((builder) => whereBuilder(filter, builder))
+      .groupBy('planter_first_name', 'planter_last_name', 'planter_identifier')
+      .orderBy('count', 'desc')
+      .limit(options.limit)
+      .offset(options.offset);
+
+    const averageTreePerPlanterQuery = knex(this._tableName)
+      .avg('totalPlanters')
+      .from(function () {
+        this.count('* as totalPlanters')
+          .from('tree_denormalized')
+          .where((builder) => whereBuilder(filter, builder))
+          .groupBy(
+            'planter_first_name',
+            'planter_last_name',
+            'planter_identifier',
+          )
+          .as('planters');
+      });
+
+    const totalSpeciesQuery = knex(this._tableName)
+      .where((builder) => whereBuilder(filter, builder))
+      .countDistinct('species as totalSpecies');
+
+    const topSpeciesQuery = knex(this._tableName)
+      .select(knex.raw('species, count(*) as count'))
+      .where((builder) => whereBuilder(filter, builder))
+      .whereNotNull('species')
+      .groupBy('species')
+      .orderBy('count', 'desc')
+      .limit(options.limit)
+      .offset(options.offset);
+
+    const averageTreesPerPlanterPerOrganizationQuery = knex(this._tableName)
+      .avg('averageTreesPerPlanters')
+      .from(function () {
+        this.avg('count as averageTreesPerPlanters')
           .from(function () {
             this.select(
               knex.raw(
@@ -213,68 +180,93 @@ class TreeRepository extends BaseRepository {
               .as('plantersCount');
           })
           .groupBy('planting_organization_name', 'planting_organization_uuid')
-          .orderBy('averagetreesperplanters', 'desc')
-          .limit(options.limit)
-          .offset(options.offset);
-    
-        const lastUpdatedQuery = knex(this._tableName).max('created_at');
-    
-        if (filter?.card_title) {
-          const { card_title } = filter;
-    
-          switch (card_title) {
-            case 'planters': {
-              const topGrowersPerOrganizatino =
-                await topGrowersPerOrganizatinoQuery.cache();
-              return { topGrowersPerOrganizatino };
-            }
-            case 'species': {
-              const topSpecies = await topSpeciesQuery.cache();
-              return { topSpecies };
-            }
-            case 'top_planters': {
-              const topPlanters = await topPlantersQuery.cache();
-              return { topPlanters };
-            }
-            case 'trees_per_planters': {
-              const topAverageTreesPerPlanterPerOrganization =
-                await topAverageTreesPerPlanterPerOrganizationQuery.cache();
-              return { topAverageTreesPerPlanterPerOrganization };
-            }
-    
-            default:
-              break;
-          }
-        }
-    
-        const totalGrowers = await totalGrowersQuery.cache();
-        const topPlanters = await topPlantersQuery.cache();
-        const averageTreePerPlanter =
-          await averageTreePerPlanterQuery.cache();
-        const topGrowersPerOrganizatino =
-          await topGrowersPerOrganizatinoQuery.cache();
-        const totalSpecies = await totalSpeciesQuery.cache();
-        const topSpecies = await topSpeciesQuery.cache();
-        const averageTreesPerPlanterPerOrganization =
-          await averageTreesPerPlanterPerOrganizationQuery.cache();
-        const topAverageTreesPerPlanterPerOrganization =
-          await topAverageTreesPerPlanterPerOrganizationQuery.cache();
-        const lastUpdated = await lastUpdatedQuery.cache();
-    
-        return {
-          totalGrowers: +totalGrowers[0].totalPlanters,
-          topPlanters,
-          averageTreePerPlanter: +averageTreePerPlanter[0].avg,
-          topGrowersPerOrganizatino,
-          totalSpecies: +totalSpecies[0].totalSpecies,
-          topSpecies,
-          averageTreesPerPlanterPerOrganization:
-            averageTreesPerPlanterPerOrganization[0].avg,
-          topAverageTreesPerPlanterPerOrganization,
-          lastUpdated: lastUpdated[0].max,
-        };
-      }
+          .as('plantersAverage');
+      });
 
+    const topAverageTreesPerPlanterPerOrganizationQuery = knex(this._tableName)
+      .select(
+        knex.raw(
+          'planting_organization_name, avg(count) as averageTreesPerPlanters',
+        ),
+      )
+      .from(function () {
+        this.select(
+          knex.raw(
+            `count(*) as count, planting_organization_name, planting_organization_uuid`,
+          ),
+        )
+          .from('tree_denormalized')
+          .where((builder) => whereBuilder(filter, builder))
+          .groupBy(
+            'planting_organization_uuid',
+            'planting_organization_name',
+            'planter_first_name',
+            'planter_last_name',
+            'planter_identifier',
+          )
+          .as('plantersCount');
+      })
+      .groupBy('planting_organization_name', 'planting_organization_uuid')
+      .orderBy('averageTreesPerPlanters', 'desc')
+      .limit(options.limit)
+      .offset(options.offset);
+
+    const lastUpdatedQuery = knex(this._tableName).max('created_at');
+
+    if (filter?.card_title) {
+      const { card_title } = filter;
+
+      switch (card_title) {
+        case 'planters': {
+          const topGrowersPerOrganizatino =
+            await topGrowersPerOrganizatinoQuery.cache();
+          return { topGrowersPerOrganizatino };
+        }
+        case 'species': {
+          const topSpecies = await topSpeciesQuery.cache();
+          return { topSpecies };
+        }
+        case 'top_planters': {
+          const topPlanters = await topPlantersQuery.cache();
+          return { topPlanters };
+        }
+        case 'trees_per_planters': {
+          const topAverageTreesPerPlanterPerOrganization =
+            await topAverageTreesPerPlanterPerOrganizationQuery.cache();
+          return { topAverageTreesPerPlanterPerOrganization };
+        }
+
+        default:
+          break;
+      }
+    }
+
+    const totalGrowers = await totalGrowersQuery.cache();
+    const topPlanters = await topPlantersQuery.cache();
+    const averageTreePerPlanter = await averageTreePerPlanterQuery.cache();
+    const topGrowersPerOrganizatino =
+      await topGrowersPerOrganizatinoQuery.cache();
+    const totalSpecies = await totalSpeciesQuery.cache();
+    const topSpecies = await topSpeciesQuery.cache();
+    const averageTreesPerPlanterPerOrganization =
+      await averageTreesPerPlanterPerOrganizationQuery.cache();
+    const topAverageTreesPerPlanterPerOrganization =
+      await topAverageTreesPerPlanterPerOrganizationQuery.cache();
+    const lastUpdated = await lastUpdatedQuery.cache();
+
+    return {
+      totalGrowers: +totalGrowers[0].totalPlanters,
+      topPlanters,
+      averageTreePerPlanter: +averageTreePerPlanter[0].avg,
+      topGrowersPerOrganizatino,
+      totalSpecies: +totalSpecies[0].totalSpecies,
+      topSpecies,
+      averageTreesPerPlanterPerOrganization:
+        averageTreesPerPlanterPerOrganization[0].avg,
+      topAverageTreesPerPlanterPerOrganization,
+      lastUpdated: lastUpdated[0].max,
+    };
+  }
 }
 
 module.exports = TreeRepository;
